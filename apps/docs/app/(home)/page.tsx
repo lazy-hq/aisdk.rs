feb826303b899a7000752ffd1fc40d3b83117e68
ishaksebsib <ishaksebsib@gmail.com>
2025-10-05 08:07:58 +0300

    init(docs): add intial docs page setup with fumadocs

---
+++ apps/docs/app/(home)/page.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import { TITLE } from "@/lib/layout.shared";
+
+export default function HomePage() {
+	return (
+		<main className="flex flex-1 flex-col justify-center text-center">
+			<h1 className="mb-4 text-2xl font-bold">Welcome to {TITLE}</h1>
+			<p className="text-fd-muted-foreground">
+				You can open{" "}
+				<Link
+					href="/docs"
+					className="text-fd-foreground font-semibold underline"
+				>
+					/docs
+				</Link>{" "}
+				and see the documentation.
+			</p>
+		</main>
+	);
+}
