2e4179d57feb2841b7c607fb1e3e43f3e7b66817
ishaksebsib <ishaksebsib@gmail.com>
2025-10-12 13:31:57 +0300

    docs: add getting started page

--- apps/docs/mdx-components.tsx
+++ apps/docs/mdx-components.tsx
@@ -1,10 +1,17 @@
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import type { MDXComponents } from "mdx/types";
+import { Type, ImageIcon, Music, Grid, Workflow, Brain } from "lucide-react";
 
 // use this function to get MDX components, you will need it for rendering MDX
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
 	return {
 		...defaultMdxComponents,
+		Workflow,
+		Brain,
+		Type,
+		ImageIcon,
+		Music,
+		Grid,
 		...components,
 	};
 }
